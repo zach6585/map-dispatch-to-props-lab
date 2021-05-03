81913614285cee19daad05fefd7b7e4878295ce0
zach6585 <zmines@wesleyan.edu>
2021-05-03 16:06:17 -0400

    commit tree 1b27446a8314f6c6e1d51b7cc007ba5685c516ba

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant()
   }
 
   render() {
@@ -49,6 +49,19 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapStateToProps = (state) => {
+  return {
+    restaurants: state.restaurants
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: () => {
+      dispatch(addRestaurant())
+    }
+  }
+}
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(mapStateToProps,mapDispatchToProps)(RestaurantInput)
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 class Restaurants extends Component {
 
   render() {
-
+    debugger
     let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
 
     return (
